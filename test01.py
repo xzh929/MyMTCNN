f2e23ee8ebfd5d368951ae1734ee909dbf0cc63f
fallen <921352686@qq.com>
2022-04-22 11:23:49 +0800

    update

---
+++ test01.py
@@ -0,0 +1,5 @@
+import cv2
+cv2.getRotationMatrix2D()
+cv2.bilateralFilter()
+cv2.drawContours()
+cv2.multiply()
---
+++ train_onet.py
@@ -0,0 +1,10 @@
+# O网络训练
+
+import nets
+import train
+
+if __name__ == '__main__':
+    net = nets.ONet()
+
+    trainer = train.Trainer(net, './param/onet.pt', r"F:\data\48") # 网络，保存参数，训练数据；创建训器
+    trainer.train()                                                     # 调用训练器中的train方法
