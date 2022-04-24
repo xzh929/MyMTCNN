03c703b5c265ec010c8c43f08aa38c377d11ccb7
fallen <921352686@qq.com>
2022-04-24 18:03:16 +0800

    update black

--- black_pad.py
+++ black_pad.py
@@ -4,14 +4,22 @@ import torch
 import numpy as np
 from torchvision import transforms as t
 
-img = Image.open(r"F:\imgdata\52.jpg")
-w, h = img.size
-max_size = np.max((h, w))
-min_size = np.min((h, w))
-if max_size == h:
-    img_mask = t.Pad([(max_size - min_size), 0, 0, 0])(img)
-else:
-    img_mask = t.Pad([0, (max_size - min_size), 0, 0, ])(img)
-img_mask.show()
-print(img.size)
-print(img_mask.size)
+def black_pad(path):
+    img = Image.open(path)
+    w, h = img.size
+    max_size = np.max((h, w))
+    min_size = np.min((h, w))
+    if max_size == h:
+        img_mask = t.Pad([(max_size - min_size), 0, 0, 0])(img)
+    else:
+        img_mask = t.Pad([0, (max_size - min_size), 0, 0, ])(img)
+    return img_mask
+
+
+if __name__ == '__main__':
+    img_path = r"F:\imgdata\52.jpg"
+    img = Image.open(img_path)
+    img_blackpad = black_pad(img_path)
+    img_blackpad.show()
+    print(img.size)
+    print(img_blackpad.size)
